602e720ab8412f8ab45f68808b0c0e09c91b3efb
YAOrangeTime16 <yokoandrae@gmail.com>
2016-10-25 15:39:23 +0200

    code completed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-
 /**
  * Här får du skapa dina låtar samt din playlist samt lägga till låtarna i playlisten.
  * Använd constructorn för Playlist samt Song för att skapa en playlist och låtar, sedan lägg till
  * låtarna med prototypmetoden "add" som du skapat.
  */
 
+var myPlaylist = new Playlist();
+
+var mysong1 = new Song("It\'s my life","Bon Jovi");
+var mysong2 = new Song("Perfect", "Lady G.");
+var mysong3 = new Song("Maybe You", "Bob D.");
 
 
 
-
-
+myPlaylist.add(mysong1);
+myPlaylist.add(mysong2);
+myPlaylist.add(mysong3);
--- js/playlist.js
+++ js/playlist.js
@@ -1,32 +1,48 @@
 
 function Playlist(){
     //Playlisten ska inte ta några argument men ska ha följande variabler:
-    //songs som är en array
-    //isPlaying som är en bool
-    //playingIndex som är 0
-}
+    this.songs = [];
+    this.isPlaying = false;
+    this.playingIndex = 0;
 
+    
+}
 Playlist.prototype.add = function(song){
     //Denna funktion ska lägga till en ny låt till playlisten
+    this.song = song;
+    this.songs.push(this.song);
+    
 };
 
 Playlist.prototype.play = function(){
     //Denna funktion ska starta playlisten och låten
+    this.isPlaying = true;
+    return this.getCurrent;
 };
 
 
 Playlist.prototype.stop = function(){
     //Denna funktion ska stoppa låten samt stoppa playlisten
+    this.isPlaying = false;
+
 };
 
 Playlist.prototype.next = function(){
     //Denna funktion ska byta låt. Om vi är på sista låten ska
     //funktionen gå tillbaka till första låten om vi anropar funktionen medan
     //vi står på sista låten
+    if (this.playingIndex == this.songs.length -1) {
+        this.playingIndex = 0;
+    }
+    else {
+        this.playingIndex +=1;
+    }
+    return this.songs[this.playingIndex];
 };
 
 Playlist.prototype.getCurrent = function(){
     //Returnera nuvarande låten
+    return this.songs[this.playingIndex];
 };
 
 
